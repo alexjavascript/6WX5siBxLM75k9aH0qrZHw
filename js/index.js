38c1cf50d70390652405a3cd7716b7929dff7f98
Alex. <grigoriev.alex.97@gmail.com>
2024-07-08 16:03:57 +0500

    Implement theming based on system preferences

--- js/index.js
+++ js/index.js
@@ -1,11 +1,36 @@
 const THEME_ATTRIBUTE = 'data-theme';
+
 const Themes = {
   LIGHT: 'light',
   DARK: 'dark',
 };
 
+const setDarkTheme = () => root.setAttribute(THEME_ATTRIBUTE, Themes.DARK);
+const setLightTheme = () => root.setAttribute(THEME_ATTRIBUTE, Themes.LIGHT);
+
+const themeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+const setSystemTheme = () => {
+  const isSystemDark = themeMediaQuery.matches;
+
+  if (isSystemDark) {
+    setDarkTheme();
+    return;
+  }
+
+  setLightTheme();
+}
+
+setSystemTheme();
+
+const handleThemeMediaQueryChange = () => {
+  setSystemTheme();
+};
+
+themeMediaQuery.addEventListener('change', setSystemTheme);
+
 const root = document.documentElement;
-const button = document.querySelector('#theme-toggle-button')
+const button = document.querySelector('#theme-toggle-button');
 
 const toggleTheme = () => {
   const theme = root.getAttribute(THEME_ATTRIBUTE);
